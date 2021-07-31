afa0c72dadf03ee5b7942074c431073c66282d59
Pierre Rolland <roll.pierre@gmail.com>
2021-07-31 16:44:12 +0200

    Added gesture and keys detectors to handle play

--- lib/views/pages/video.dart
+++ lib/views/pages/video.dart
@@ -4,6 +4,17 @@ import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 import 'package:wakelock/wakelock.dart';
 
+final rewindKeySet = LogicalKeySet(
+  LogicalKeyboardKey.arrowLeft,
+);
+
+final forwardKeySet = LogicalKeySet(
+  LogicalKeyboardKey.arrowRight
+);
+
+class RewindIntent extends Intent {}
+class ForwardIntent extends Intent {}
+
 class VideoPage extends StatefulWidget {
   const VideoPage({Key? key, required this.videoUrl}) : super(key: key);
 
@@ -16,6 +27,7 @@ class VideoPage extends StatefulWidget {
 class _VideoPageState extends State<VideoPage> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+  late double _lastDoubleTapX;
 
   @override
   void initState() {
@@ -46,40 +58,61 @@ class _VideoPageState extends State<VideoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              _controller.play();
-            }
-          });
+    void _pauseOrPlay() {
+      setState(() {
+        if (_controller.value.isPlaying) {
+          _controller.pause();
+        } else {
+          _controller.play();
+        }
+      });
+    }
+    void _rewind() async {
+      await _controller.seekTo(((await _controller.position) ?? const Duration()) - const Duration(seconds: 30));
+    }
+    void _forward () async {
+      await _controller.seekTo(((await _controller.position) ?? const Duration()) + const Duration(seconds: 30));
+    }
+
+    return GestureDetector(
+      onTap: _pauseOrPlay,
+      onDoubleTapDown: (TapDownDetails details) => {
+        _lastDoubleTapX = details.globalPosition.dx
+      },
+      onDoubleTap: () => {
+        _lastDoubleTapX < MediaQuery.of(context).size.width / 2 ? _rewind() : _forward()
+      },
+      child: FocusableActionDetector(
+        autofocus: true,
+        shortcuts: {
+          rewindKeySet: RewindIntent(),
+          forwardKeySet: ForwardIntent()
         },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-        mini: true,
-        backgroundColor: Colors.transparent,
+        actions: {
+          RewindIntent: CallbackAction(onInvoke: (_) => _rewind),
+          ForwardIntent: CallbackAction(onInvoke: (_) => _forward),
+          ActivateIntent: CallbackAction(onInvoke: (_) => _pauseOrPlay),
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+              child: FutureBuilder(
+                future: _initializeVideoPlayerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              )
+          ),
+        )
       ),
     );
   }
